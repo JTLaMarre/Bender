5449522625bf04fb02e6966a5651963192b2c6c8
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-10-04 11:32:00 -0400

    mapping over question set to generate a question list with buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import questions from './questions.json';
+import Question from './components/question'
 
 function App() {
   return (
     <Container>
 
     <div className="App">
-      
+      {questions.map((question)=>{
+        return(
+          <Question key={question.id} question={question.question} A={question.A} B={question.B} C={question.C} D={question.C} />
+        )
+      })}
     </div>
     </Container>
   );
--- client/src/components/question.js
+++ client/src/components/question.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
+const Question = (props) => {
+    return (
+        <Row>
+            <Col sm={4}>
+                <h3>{props.question}</h3>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <Button variant="outline-secondary">A</Button>
+                    </InputGroup.Prepend>
+                    <InputGroup.Text>{props.A}</InputGroup.Text>
+                   
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <Button variant="outline-secondary">B</Button>
+                    </InputGroup.Prepend>
+                    <InputGroup.Text>{props.B}</InputGroup.Text>
+                    
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <Button variant="outline-secondary">C</Button>
+                    </InputGroup.Prepend>
+                    <InputGroup.Text>{props.C}</InputGroup.Text>
+                   
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <Button variant="outline-secondary">D</Button>
+                        <InputGroup.Text>{props.D}</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    
+                </InputGroup>
+            </Col>
 
-const Question =()=>{
-    return(
-        <div>
-
-        </div>
+        </Row>
     )
 }
 
